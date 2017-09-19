11543d821339dc5997f9cfa3e87f8f93248fe667
ChangJoo Park <pcjpcj2@gmail.com>
2017-09-19 22:48:19 +0900

    Add at-ui menu

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <at-menu mode="horizontal" active-Name="1">
+      <at-menu-item name="1"><i class="icon icon-home"></i>Navigation One</at-menu-item>
+      <at-menu-item name="2" disabled><i class="icon icon-layers"></i>Navigation Two</at-menu-item>
+      <at-submenu>
+        <template slot="title"><i class="icon icon-life-buoy"></i>Navigation Three - Submenu</template>
+        <at-menu-item-group title="Group One">
+          <at-menu-item name="3-1">Submenu One</at-menu-item>
+          <at-menu-item name="3-2" disabled>Submenu Two</at-menu-item>
+        </at-menu-item-group>
+        <at-menu-item-group title="Group Two">
+          <at-menu-item name="3-3">Submenu Three</at-menu-item>
+          <at-menu-item name="3-4">Submenu Four</at-menu-item>
+        </at-menu-item-group>
+      </at-submenu>
+      <at-menu-item name="4"><i class="icon icon-settings"></i>Navigation Four</at-menu-item>
+    </at-menu>
     <router-view></router-view>
   </div>
 </template>
@@ -12,12 +27,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
